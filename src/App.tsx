0363f4d681a5759605958d378d6cf029c4a023d5
DManthonyx <40186480+DManthonyx@users.noreply.github.com>
2023-09-16 08:35:11 -0700

    add packages classnames react-router-dom styled-icons tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "@pages/home/home";
 import Dashboard from "@pages/dashboard/dashboard";
+import LogIn from "@pages/log-in/log-in";
 
 function App() {
   return (
-    <div className="main-wrapper">
+    <div className="main-wrapper flex">
       <div className="main-header">
         <Link to="/">Home</Link>
         <Link to="/dashboard">Dashboard</Link>
+        <Link to="/login">Log In</Link>
       </div>
       <Routes>
+        <Route path="/login" element={<LogIn />} />
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
